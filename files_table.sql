ed13fc936149e323ac345b930b44263e89f3b077
Mohamed Yousef <modi401@hotmail.com>
2018-09-20 11:27:30 +0200

    Create files_table.sql

---
+++ files_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE files(
+    file_id INT AUTO_INCREMENT,
+    file_name VARCHAR(150),
+    file_link VARCHAR(255),
+    file_size Double,
+    file_date DATE,
+    PRIMARY KEY (file_id)
+);
